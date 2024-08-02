df333dd4406ec531f09357d424d45ab0123ea5ac
daniel alessi <danielalessi@daniels-MacBook-Air.local>
2024-08-02 17:42:51 -0400

    added setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='minbit',
+    version='0.0.0',
+    packages=find_packages(),
+    install_requires=[
+        'click'
+    ],
+    entry_points='''
+    [console_scripts]
+    minbit=minbit:minbit'''
+)
